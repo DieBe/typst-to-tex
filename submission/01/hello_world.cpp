7d62c3848f734e40526626694849059ee3db05d0
DieBe <84198693+DieBe@users.noreply.github.com>
2026-04-13 09:30:58 +0200

    Delete submission directory/cleanup

--- submission/01/hello_world.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-#include <stdlib.h>
-
-int main() {
-    std::string name = getenv("HOSTNAME");
-    std::cout << "Hello World from " << name << std::endl;
-    return 0;
-}
